63d8268971a3adb9ddeb71dea7f11b06a46d38fc
Savio Nicodemos <saviogomes_182@hotmail.com>
2021-01-12 00:36:54 -0300

    Concluido desafio de fundamentos

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,53 @@ interface Balance {
   total: number;
 }
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balance: Balance;
+
   constructor() {
     this.transactions = [];
+    this.balance = { income: 0, outcome: 0, total: 0 };
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: Request): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    if (
+      transaction.type === 'outcome' &&
+      this.balance.total < transaction.value
+    ) {
+      throw Error('Saldo insuficiente para essa operação');
+    }
+
+    this.transactions.push(transaction);
+
+    if (transaction.type === 'income') {
+      this.balance.total += value;
+      this.balance.income += value;
+    } else if (transaction.type === 'outcome') {
+      this.balance.outcome += value;
+      this.balance.total -= value;
+    } else {
+      throw Error('Operação inválida: Aceito apenas income e outcome!');
+    }
+
+    return transaction;
   }
 }
 
